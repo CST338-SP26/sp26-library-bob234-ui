a73b4ebd0a379fc76f5ce9266fe47cce370f3af5
bob234-ui <emiliano.gomez2034@gmail.com>
2026-04-09 11:26:25 -0700

    Add convertInt and convertDate methods

--- src/main/java/library.java
+++ src/main/java/library.java
@@ -20,4 +20,68 @@ public class Library {
     }
 
     public String getName() { return name; }
+
+    public static int convertInt(String recordCountString, Code code) {
+        try {
+            return Integer.parseInt(recordCountString);
+        } catch (NumberFormatException e) {
+            System.out.println("Value which caused the error: " + recordCountString);
+            System.out.println("Error message: " + code.getMessage());
+            switch (code) {
+                case BOOK_COUNT_ERROR:
+                    System.out.println("Error: Could not read number of books");
+                    break;
+                case PAGE_COUNT_ERROR:
+                    System.out.println("Error: could not parse page count");
+                    break;
+                case DATE_CONVERSION_ERROR:
+                    System.out.println("Error: Could not parse date component");
+                    break;
+                default:
+                    System.out.println("Error: Unknown conversion error");
+                    break;
+            }
+            return code.getCode();
+        }
+    }
+
+    public static LocalDate convertDate(String date, Code errorCode) {
+        if (date.equals("0000")) {
+            return LocalDate.of(1970, 1, 1);
+        }
+
+        String[] parts = date.split("-");
+        if (parts.length != 3) {
+            System.out.println("ERROR: date conversion error, could not parse " + date);
+            System.out.println("Using default date (01-jan-1970)");
+            return LocalDate.of(1970, 1, 1);
+        }
+
+        int year  = convertInt(parts[0], Code.DATE_CONVERSION_ERROR);
+        int month = convertInt(parts[1], Code.DATE_CONVERSION_ERROR);
+        int day   = convertInt(parts[2], Code.DATE_CONVERSION_ERROR);
+
+        if (year < 0 || month < 0 || day < 0) {
+            System.out.println("Error converting date: Year " + year);
+            System.out.println("Error converting date: Month " + month);
+            System.out.println("Error converting date: Dat " + day);
+            System.out.println("Using default date (01-jan-1970)");
+            return LocalDate.of(1970, 1, 1);
+        }
+
+        return LocalDate.of(year, month, day);
+    }
+
+    public static int getLibraryCardNumber() {
+        return libraryCard + 1;
+    }
+
+    private Code errorCode(int codeNumber) {
+        for (Code code : Code.values()) {
+            if (code.getCode() == codeNumber) {
+                return code;
+            }
+        }
+        return Code.UNKNOWN_ERROR;
+    }
 }
