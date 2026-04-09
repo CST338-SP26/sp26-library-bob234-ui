5620c33bc5194fec6c7dd6fa42e4cdb6e4cfa13e
bob234-ui <emiliano.gomez2034@gmail.com>
2026-04-09 11:19:19 -0700

    Add Library fields and constructor

--- src/main/java/library.java
+++ src/main/java/library.java
@@ -1,2 +1,23 @@
-public class library {
+import Utilities.Code;
+import java.util.*;
+import java.time.LocalDate;
+
+public class Library {
+
+    public static final int LENDING_LIMIT = 5;
+
+    private String name;
+    private static int libraryCard = 0;
+    private List<Reader> readers;
+    private HashMap<String, Shelf> shelves;
+    private HashMap<Book, Integer> books;
+
+    public Library(String name) {
+        this.name = name;
+        this.readers = new ArrayList<>();
+        this.shelves = new HashMap<>();
+        this.books = new HashMap<>();
+    }
+
+    public String getName() { return name; }
 }
